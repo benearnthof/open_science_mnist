afd503e2da54e7d1babf476070ce50777fc7d23e
benearnthof <benearnthof@hotmail.de>
2023-01-11 21:04:29 +0100

    add models.py

---
+++ models.py
@@ -0,0 +1,46 @@
+# This file specifies the model architectures used for our experiments
+# Convolutional Net with Dropout
+# Simple Fully connected Net
+
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ConvNet(nn.Module):
+  def __init__(self):
+    super(ConvNet, self).__init__()
+    self.conv1 = nn.Conv2d(1, 32, 3, 1)
+    self.conv2 = nn.Conv2d(32, 64, 3, 1)
+    self.dropout1 = nn.Dropout2d(0.25)
+    self.dropout2 = nn.Dropout2d(0.5)
+    self.fc1 = nn.Linear(9216, 128)
+    self.fc2 = nn.Linear(128, 10)
+
+  def forward(self, x):
+    x = self.conv1(x)
+    x = F.relu(x)
+    x = self.conv2(x)
+    x = F.max_pool2d(x, 2)
+    x = self.dropout1(x)
+    x = torch.flatten(x, 1)
+    x = self.fc1(x)
+    x = F.relu(x)
+    x = self.dropout2(x)
+    x = self.fc2(x)
+    output = F.log_softmax(x, dim=1)
+    return output
+
+class FCNet(nn.Module):
+  def __init__(self):
+    super(FCNet, self).__init__()
+    self.fc1 = nn.Linear(28*28, 512)
+    self.fc2 = nn.Linear(512, 512)
+    self.fc3 = nn.Linear(512, 10)
+  
+  def forward(self, x):
+    x = x.view(-1, 28*28)
+    x = F.relu(self.fc1(x))
+    x = F.relu(self.fc2(x))
+    x = F.relu(self.fc3(x))
+    output = F.log_softmax(x, dim=1)
+    return output
